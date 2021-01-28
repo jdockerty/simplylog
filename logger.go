58fcea14bca382f5a42153f4b80f966a7418d085
Jack Dockerty <jdockerty19@gmail.com>
2021-01-28 22:41:30 +0000

    Updating comments and New func.

--- logger.go
+++ logger.go
@@ -8,21 +8,29 @@ import (
 
 // Logger is the core component of simplylog.
 type Logger struct {
-	Out io.Writer
-	// Format Format
+
+	// Format provides configuration options for a few simple values that are used
+	// within the package.
+	Format Format
 
 	// Verbose is used to set whether the logging output should be set to 'Debug' or not.
-	// As such, setting verbose to true is akin to having the logging level set as 'Debug', 
+	// As such, setting verbose to true is akin to having the logging level set as 'Debug',
 	// with false being 'Informational'. This follows Dave Cheney's dicussion on Golang logging.
 	Verbose bool
+
+	// Out is the output
+	Out io.Writer
 
 	m sync.Mutex
 }
 
-
 // New will create a new logger with the default settings.
 func New() *Logger {
 	return &Logger{
+		Format: Format{
+			Timestamp: "15:04:05 02/01/2006",  // Default Go reference time
+			Type: "text",
+		},
 		Out: os.Stderr,
 		Verbose: false,
 	}
