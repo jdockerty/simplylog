a11ae7e5b211adcc3f7fd74d650da7a2f52638c0
Jack Dockerty <jdockerty19@gmail.com>
2021-01-28 08:58:34 +0000

    Updating New func test.

--- logger_test.go
+++ logger_test.go
@@ -1,11 +1,22 @@
 package simplylog_test
 
 import (
+	"io"
 	"testing"
+
 	"github.com/jdockerty/simplylog"
+	"github.com/stretchr/testify/assert"
 )
 
 
-func TestFunc(t *testing.T) {
-	simplylog.New()
+func TestNewAreDefaults(t *testing.T) {
+	assert := assert.New(t)
+	logger := simplylog.New()
+
+	assert.Implements((*io.Writer)(nil), logger.Out, "Out must implement the io.Writer interface.")
+	
+}
+
+func TestSetOutputChanged(t *testing.T) {
+
 }
